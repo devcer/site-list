70ca8805c17f662aa833ad5f7149ba3d57a25fa3
devcer <viswanathamsantosh@gmail.com>
2021-01-26 14:49:26 +0530

    Added custom data types to site and site details

--- src/app/pages/site/site.component.ts
+++ src/app/pages/site/site.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AlertService } from 'src/app/services/alert/alert.service';
 import { SitesService } from 'src/app/services/sites/sites.service';
+import { ISiteDetails } from '../sites-list/sites-list.component';
 
 @Component({
   selector: 'app-site',
@@ -10,7 +11,17 @@ import { SitesService } from 'src/app/services/sites/sites.service';
 })
 export class SiteComponent implements OnInit {
   siteId = '';
-  siteDetails: any;
+  siteDetails: ISiteDetails = {
+    id: '',
+    imgSrc: '',
+    siteName: '',
+    email: '',
+    phoneNumber: '',
+    jobTitle: '',
+    shortAddress: '',
+    fullAddress: '',
+    mainContact: '',
+  };
   showDetailSection = false;
   showProgressBar = true;
   constructor(
@@ -25,7 +36,17 @@ export class SiteComponent implements OnInit {
     this.siteService.getSiteDetails(siteId).subscribe(
       (data) => {
         console.log(data);
-        this.siteDetails = data;
+        this.siteDetails = {
+          id: data.id,
+          imgSrc: data.images[0],
+          siteName: data.title,
+          email: data.contacts.main.email,
+          phoneNumber: data.contacts.main.phoneNumber,
+          jobTitle: data.contacts.main.jobTitle,
+          shortAddress: `${data.address.state}, ${data.address.country}`,
+          fullAddress: `${data.address.street}, ${data.address.city}, ${data.address.state}, ${data.address.country} - ${data.address.zipCode}`,
+          mainContact: `${data.contacts.main.firstName} ${data.contacts.main.lastName}`,
+        };
         this.showDetailSection = true;
       },
       (error) => {
--- src/app/pages/sites-list/sites-list.component.ts
+++ src/app/pages/sites-list/sites-list.component.ts
@@ -9,14 +9,27 @@ import { SitesService } from 'src/app/services/sites/sites.service';
   styleUrls: ['./sites-list.component.scss'],
 })
 export class SitesListComponent implements OnInit {
-  sitesData: any[] = [];
+  sitesData: ISite[] = [];
   showProgressBar = true;
-  constructor(private siteService: SitesService, private router: Router, private alert: AlertService) {}
+  constructor(
+    private siteService: SitesService,
+    private router: Router,
+    private alert: AlertService
+  ) {}
 
   ngOnInit(): void {
     this.siteService.getSitesList().subscribe(
       (data) => {
-        this.sitesData = data as any[];
+        this.sitesData = data.map((site: any) => {
+          return {
+            id: site.id,
+            imgSrc: site.images[0],
+            siteName: site.title,
+            shortAddress: `${site.address.state}, ${site.address.country}`,
+            mainContact: `${site.contacts.main.firstName} ${site.contacts.main.lastName}`,
+          };
+        });
+        console.log(this.sitesData);
         this.showProgressBar = false;
       },
       (error) => {
@@ -28,4 +41,24 @@ export class SitesListComponent implements OnInit {
   goToSite(siteId: string): void {
     this.router.navigateByUrl(`sites/${siteId}`);
   }
+}
+
+export interface ISite {
+  id: string;
+  imgSrc: string;
+  siteName: string;
+  shortAddress: string;
+  fullAddress: string;
+  mainContact: string;
+}
+export interface ISiteDetails {
+  id: string;
+  imgSrc: string;
+  siteName: string;
+  email: string;
+  phoneNumber: string;
+  jobTitle: string;
+  shortAddress: string;
+  fullAddress: string;
+  mainContact: string;
 }
